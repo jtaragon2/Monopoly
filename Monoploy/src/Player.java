2764a05922b36650a082c1eb052f676b12f52bfc
jonathan.aragon <jonathan.aragon@MU-C1-15.mullen.pvt.k12.co.us>
2022-01-07 08:40:25 -0700

    Friday Work

--- Monoploy/src/Player.java
+++ Monoploy/src/Player.java
@@ -1,8 +1,45 @@
+import java.util.ArrayList;
 
 public class Player
 	{
 		private int balance;
-		public Player(int b) {
-			balance = b;
+		private int location;
+		ArrayList<Property> ownedProperties = new ArrayList<Property>();
+
+		public Player()
+			{
+				balance = 1500;
+			}
+
+		public ArrayList<Property> getOwnedProperties()
+			{
+				return ownedProperties;
+			}
+
+		public void setOwnedProperties(ArrayList<Property> ownedProperties)
+			{
+				this.ownedProperties = ownedProperties;
+			}
+
+		public int getBalance()
+			{
+				return balance;
+			}
+		public void addToBalance(int b) {
+			balance += b;
 		}
+		public void subtractFromBalance(int b) {
+			balance -= b;
+		}
+
+		public int getLocation()
+			{
+				return location;
+			}
+
+		public void setLocation(int location)
+			{
+				this.location = location;
+			}
+		
 	}
