556836790a27fd2605d3188410ca89ae05141af1
benjamin.hoody <benjamin.hoody@MU-C1-17.mullen.pvt.k12.co.us>
2022-01-12 13:43:18 -0700

    finished switch structure

--- Monoploy/src/Monopoly.java
+++ Monoploy/src/Monopoly.java
@@ -5,13 +5,22 @@ public class Monopoly
 	{
 
 		static Player player1 = new Player();
-		
+	
 		public static void main(String[] args)
 			{
 				Spaces.fillboard();
 				System.out.println("Welcome to Monopoly your staring balance is " + player1.getBalance() + ".\nPress Enter to Role the dice!" );
 				pause();
 				
+				boolean h = true;
+				
+				while(h)
+					{
+						movePlayer();
+						checkLocation();
+						pause();
+					}
+
 				
 			}
 		
@@ -62,8 +71,13 @@ public class Monopoly
 						}
 				case 5:
 						{
-							System.out.println("Income Tax: Pay $100 to the bank!");
 							player1.subtractFromBalance(100);
+							System.out.println("Income Tax: Pay $100 to the bank!\nYour balance is now "+player1.getBalance()+"!");
+							break;
+						}
+				case 6:
+						{
+							System.out.println("You have landed at the " + Spaces.board.get(5).getName() + "!");
 							break;
 						}
 				case 7:
@@ -86,9 +100,19 @@ public class Monopoly
 							System.out.println("You have landed at " + Spaces.board.get(9).getName() + "!");
 							break;
 						}
+				case 11:
+						{
+							System.out.println("You have landed at the " + Spaces.board.get(10).getName() + "! You are just visiting!");
+							break;
+						}
 				case 12:
 						{
 							System.out.println("You have landed at " + Spaces.board.get(11).getName() + "!");
+							break;
+						}
+				case 13:
+						{
+							System.out.println("You have landed at the " + Spaces.board.get(12).getName() + "!");
 							break;
 						}
 				case 14:
@@ -99,6 +123,11 @@ public class Monopoly
 				case 15:
 						{
 							System.out.println("You have landed at " + Spaces.board.get(14).getName() + "!");
+							break;
+						}
+				case 16:
+						{
+							System.out.println("You have landed at the " + Spaces.board.get(15).getName() + "!");
 							break;
 						}
 				case 17:
@@ -121,6 +150,13 @@ public class Monopoly
 							System.out.println("You have landed at " + Spaces.board.get(19).getName() + "!");
 							break;
 						}
+				case 21:
+						{
+							player1.addToBalance(500);
+							System.out.println("Free Parking!!!!! You gained $500!\nYour balance is now " +player1.getBalance()+"!");
+							
+							break;
+						}
 				case 22:
 						{
 							System.out.println("You have landed at " + Spaces.board.get(21).getName() + "!");
@@ -141,6 +177,11 @@ public class Monopoly
 							System.out.println("You have landed at " + Spaces.board.get(24).getName() + "!");
 							break;
 						}
+				case 26:
+						{
+							System.out.println("You have landed at the " + Spaces.board.get(25).getName() + "!");
+							break;
+						}
 				case 27:
 						{
 							System.out.println("You have landed at " + Spaces.board.get(26).getName() + "!");
@@ -151,9 +192,19 @@ public class Monopoly
 							System.out.println("You have landed at " + Spaces.board.get(27).getName() + "!");
 							break;
 						}
+				case 29:
+						{
+							System.out.println("You have landed at the " + Spaces.board.get(28).getName() + "!");
+							break;
+						}
 				case 30:
 						{
 							System.out.println("You have landed at " + Spaces.board.get(29).getName() + "!");
+							break;
+						}
+				case 31:
+						{
+							System.out.println("You have done something illegal and now must go to Jail....");
 							break;
 						}
 				case 32:
@@ -176,9 +227,25 @@ public class Monopoly
 							System.out.println("You have landed at " + Spaces.board.get(34).getName() + "!");
 							break;
 						}
+				case 36:
+						{
+							System.out.println("You have landed at the " + Spaces.board.get(35).getName() + "!");
+							break;
+						}
+				case 37:
+						{
+							System.out.println("You landed on a chance card!");
+							break;
+						}
 				case 38:
 						{
 							System.out.println("You have landed at " + Spaces.board.get(37).getName() + "!");
+							break;
+						}
+				case 39:
+						{
+							player1.subtractFromBalance(75);
+							System.out.println("You have landed on " + Spaces.board.get(37).getName() + " and must pay $75 to the bank!\nYour balance is now " +player1.getBalance()+"!");
 							break;
 						}
 				case 40:
